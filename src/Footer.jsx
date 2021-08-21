e904e237a8ab6d8ac23e1e260d45069ab5c10282
Yomal Bandara <yomalb07@gmail.com>
2021-08-21 22:24:34 +0530

    Intial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer(){
+    var date =  new Date();
+    var year = date.getFullYear();
+    return <footer>
+        <p>Copyright {year}</p>
+    </footer>
+}
+
+export default Footer
